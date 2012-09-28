aae2a97a2dee4ca68c3c78f084f62b66c9244a0d
Kevin Xu <kevin@imkevinxu.com>
2012-09-28 11:08:50 -0700

    first commit

---
+++ textmemaybe_app/appurls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import *
+from django.shortcuts import redirect
+from django.contrib.auth.views import login, logout
+
+def smartlogin(request, **kwargs):
+    if request.user.is_authenticated():
+        return redirect('index')
+    else:
+        return login(request, **kwargs)
+
+urlpatterns = patterns('textmemaybe_app.views',
+    url(r'^$', 'index', name='index'),
+    url(r'^login/$', smartlogin, kwargs=dict(template_name='login.html'), name='login'),
+    url(r'^logout/$', logout, kwargs=dict(next_page='/'), name='logout'),
+
+)
